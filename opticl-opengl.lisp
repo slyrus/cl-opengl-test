1233a17cf036047d8d7d4384ad048ec49081dde0
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-03-28 22:26:31 -0700

    simplify code a bit -- redraw only when dirty and, importantly, set pixel-store :unpack-alignment to 1 so that non (mod width 4) == 0 opticl images can be displayed properly.

--- opticl-opengl.lisp
+++ opticl-opengl.lisp
@@ -4,29 +4,53 @@
 (defclass opticl-window (glut:window)
   ((texture :accessor texture)
    (framebuffer :accessor framebuffer)
-   (image :accessor image :initarg :image))
-  (:default-initargs :width 512 :height 512
+   (image :accessor image :initarg :image)
+   (dirty :accessor dirty))
+  (:default-initargs :image nil :height 128 :width 128
+                     :dirty nil
                      :title "Opticl Image Viewer"
-                     :x 100 :y 100 :mode '(:double :rgb :depth)
-                     :image (opticl:read-image-file
-                             "/Users/sly/projects/opticl-test/images/truck.jpeg")))
+                     :pos-x 100 :pos-y 100))
 
-(defmethod glut:display-window :before ((win opticl-window))
-  (let ((framebuffer (first (gl:gen-framebuffers-ext 1)))
-        (depthbuffer (first (gl:gen-renderbuffers-ext 1)))
-        (texture (first (gl:gen-textures 1))))
-    ;; setup framebuffer
-    (gl:bind-framebuffer-ext :framebuffer-ext framebuffer)
-    
-    ;; setup texture and attach it to the framebuffer
+(defun make-opticl-window (image)
+  (make-instance 'opticl-window
+                 :height (array-dimension image 0)
+                 :width (array-dimension image 1)
+                 :image image))
+
+(defun draw (window)
+  (gl:load-identity)
+  (gl:bind-framebuffer-ext :framebuffer-ext 0)
+  (gl:clear-color 0 0 0.2 0)
+  (gl:clear-depth 1)
+  (gl:clear :color-buffer-bit :depth-buffer-bit)
+  (with-accessors ((image image)) window
+    (opticl:with-image-bounds (height width) image
+      (gl:viewport (max 0 (/ (- (glut:width window) width) 2))
+                   (max 0 (/ (- (glut:height window) height) 2))
+                   width height)
+      (let ((texture (texture window)))  
+        (gl:bind-texture :texture-2d texture)
+        (gl:enable :texture-2d)
+        (gl:with-primitives :quads
+          (gl:tex-coord 0 0) (gl:vertex -1 1)
+          (gl:tex-coord 1 0) (gl:vertex 1 1)
+          (gl:tex-coord 1 1) (gl:vertex 1 -1)
+          (gl:tex-coord 0 1) (gl:vertex -1 -1))
+        (glut:swap-buffers)))))
+
+(defmethod glut:display-window :before ((window opticl-window))
+  (let ((texture (first (gl:gen-textures 1))))
+    (gl:load-identity)
+    (print (gl:get-error))
     (gl:bind-texture :texture-2d texture)
-    (gl:tex-parameter :texture-2d :texture-min-filter :linear-mipmap-linear)
-    (gl:tex-parameter :texture-2d :texture-mag-filter :linear)
-    
+    (print (gl:get-error))
     (with-accessors ((image image)
                      (window-width glut:width)
-                     (window-height glut:height)) win
+                     (window-height glut:height))
+        window
+      (setf (texture window) texture)
       (opticl:with-image-bounds (image-height image-width channels) image
+        (gl:pixel-store :unpack-alignment 1)
         (gl:tex-image-2d :texture-2d 0 :rgb image-width image-height 0 
                          (case channels
                            (3 :rgb)
@@ -35,68 +59,28 @@
                          (make-array (reduce #'* (array-dimensions image))
                                      :element-type '(unsigned-byte 8)
                                      :displaced-to image))
-
-        (gl:generate-mipmap-ext :texture-2d)
+        (print (gl:get-error))
+        (gl:generate-mipmap :texture-2d)
         (gl:bind-texture :texture-2d 0)
-        (gl:framebuffer-texture-2d-ext :framebuffer-ext
-                                       :color-attachment0-ext
-                                       :texture-2d
-                                       texture
-                                       0)
-        
-        ;; setup depth-buffer and attach it to the framebuffer
-        (gl:bind-renderbuffer-ext :renderbuffer-ext depthbuffer)
-        (gl:renderbuffer-storage-ext :renderbuffer-ext
-                                     :depth-component24
-                                     window-width
-                                     window-height)
-        (gl:framebuffer-renderbuffer-ext :framebuffer-ext
-                                         :depth-attachment-ext
-                                         :renderbuffer-ext
-                                         depthbuffer)))
-    
-    ;; validate framebuffer
-    (let ((framebuffer-status (gl:check-framebuffer-status-ext :framebuffer-ext)))
-      (unless (gl::enum= framebuffer-status :framebuffer-complete-ext)
-        (error "Framebuffer not complete: ~A." framebuffer-status)))
+        (print (gl:get-error))))
+    (draw window)))
 
-    (setf (texture win) texture
-          (framebuffer win) framebuffer)))
-
-(defmethod glut:display ((win opticl-window))
-
-  (gl:load-identity)
-  (gl:bind-framebuffer-ext :framebuffer-ext 0)
-
-  (gl:viewport 0 0 (glut:width win) (glut:height win))
-  (gl:clear-color 0 0 0.2 0)
-  (gl:clear-depth 1)
-  (gl:clear :color-buffer-bit :depth-buffer-bit)
-  
-
-  (with-accessors ((image image)) win
-    (opticl:with-image-bounds (height width) image
-    
-      (gl:viewport (max 0 (/ (- (glut:width win) width) 2))
-                   (max 0 (/ (- (glut:height win) height) 2))
-                   width height)
-
-      (let ((texture (texture win)))  
-        (gl:enable :texture-2d)
-        (gl:bind-texture :texture-2d texture)
-        (gl:push-matrix)
-
-        (gl:with-primitives :quads
-          (gl:tex-coord 0 0) (gl:vertex -1 1)
-          (gl:tex-coord 1 0) (gl:vertex 1 1)
-          (gl:tex-coord 1 1) (gl:vertex 1 -1)
-          (gl:tex-coord 0 1) (gl:vertex -1 -1))
-        (gl:pop-matrix)))
-    (glut:swap-buffers)))
-
-(defmethod glut:idle ((window opticl-window))
-  (glut:post-redisplay))
+(defmethod glut:display ((window opticl-window))
+  (with-accessors ((dirty dirty)) window
+    (when dirty
+      (draw window)
+      (setf dirty nil))))
 
 (defmethod glut:reshape ((window opticl-window) width height)
   (setf (glut:width window) width
-        (glut:height window) height))
+        (glut:height window) height)
+  (with-accessors ((dirty dirty)) window
+    (setf dirty t)))
+
+
+#+nil
+(on-initial-thread (:wait nil)
+  (let ((image (opticl:make-8-bit-rgb-image 256 257)))
+    (opticl:draw-rectangle image 50 50 200 200 255 0 0)
+    (glut:display-window (make-opticl-window image))))
+
